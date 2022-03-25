6852269e4e3bebe8d1c8800dff6e4e31cf21dafc
Ahmed Rizk <ahmadamrizk@gmail.com>
2022-03-25 00:32:07 -0700

    descriptions and argument fixes
    Quick fixes to descriptions and arguments. still incomplete

--- src/read_process_script.py
+++ src/read_process_script.py
@@ -2,7 +2,7 @@
 # date: 2022-03-24
 #
 # A script that sets a universal seed, reads the dataset used for the analysis
-# from CIS-72M0003-E-2017-Annual_F1.csv, and processes the data for the EDA.
+# from specified path, and processes the data for the EDA.
 #
 # Usage:
 
@@ -19,12 +19,12 @@ np.random.seed(1)
 
 opt = docopt(__doc__)
 
-def main(read_path, out_name, cols_kept):
+def main(read_path, out_path, cols_kept):
 
     data = pd.read_csv(read_path, header=None)
     data = data[cols_kept]
-    data.to_csv(data, index = False)
+    data.to_csv(out_path, index = False)
     
-main(opt["--read_path"], opt["--out_name"], opt[["--cols_kept"]])
+main(opt["--read_path"], opt["--out_path"], opt[["--cols_kept"]])
 
 
