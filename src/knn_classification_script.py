e41d5dcd81eccc3315728622249f8dcdbdb4f2c3
Ahmed Rizk <ahmadamrizk@gmail.com>
2022-03-28 00:37:49 -0700

    Helpers - KNN script
    Added some helpers in the KNN classification script

--- src/knn_classification_script.py
+++ src/knn_classification_script.py
@@ -5,7 +5,7 @@
 Usage:
 
    Options:
- --pro=<pro>     Path (including filename) to processed data (which needs to be saved as a csv file)
+ --processed=<processed>     Path (including filename) to processed data (which needs to be saved as a csv file)
  --out_dir=<out_dir> Path to directory where the model should be written
    
    ''' -> doc
@@ -25,14 +25,27 @@ from docopt import docopt
 
 opt = docopt(__doc__)
 
-def main(pro, out_dir):
 
-    pro['EFINVA_Made_Money'] = np.array(pro['EFINVA']) > pro['EFINVA'].median()
+def create_categ(processed, new_var, var):
+    processed[new_var] = np.array(processed[var]) > processed[var].median()
+    return processed
+
+def train_test_drop(processed, drop_col):
     train_2, test_2 = train_test_split(processed, test_size = 0.3, random_state=123)
-    X_train_2, Y_train_2, X_test_2, Y_test_2 = split_drop(processed, 0.3, 123, "EFINVA_Made_Money")
+    X_train_2, Y_train_2, X_test_2, Y_test_2 = split_drop(processed, 0.3, 123, drop_col)
+    return train_2, test_2, X_train_2, Y_train_2, X_test_2, Y_test_2  
+
+
+
+def tune_model(train1, train2):
     param_grid = {"n_neighbours": np.arange(1,50,5)}
-    results_df = KNN_tuning(preprocessor,X_train_2,Y_train_2,param_grid)
-    elbow_plt = plt.plot(results_df['n_neighbours'], 
+    results_df = KNN_tuning(preprocessor,train1,train2,param_grid) # preprocessor, which script will contain this?
+    return results_df
+
+    
+    
+def elbow_plot(results):
+    elbow_plt = plt.plot(results['n_neighbours'], 
                     results_df['mean_cv_score'], 
                     '-0')
     plt.title('Figure 4: KNN K-tuning Results')
@@ -41,9 +54,22 @@ def main(pro, out_dir):
     plt.show()
     
     
+def make_pipeline(train1, train2, test1, test2):
     pipe_final = make_pipeline(preprocessor, KNeighborsClassifier(n_neighbors=26))
-    pipe_final.fit(X_train_2, Y_train_2)
-    pipe_final.score(X_test_2, Y_test_2)
+    pipe_final.fit(train1, train2)
+    pipe_final.score(test1, test2)
+    
+
+def main(processed, out_dir):
+
+    
+    processed = create_categ(processed, 'EFINVA_Made_Money', 'EFINVA')
+    train_2, test_2, X_train_2, Y_train_2, X_test_2, Y_test_2 = train_test_drop(processed, "EFINVA_Made_Money")
+    results_df = tune_model(X_train_2,Y_train_2)
+    elbow_plot(results_df)
+    make_pipeline(X_train_2, Y_train_2, X_test_2, Y_test_2)
+    
+    
 
     
 main(opt["--train"], opt["--out_dir"])
