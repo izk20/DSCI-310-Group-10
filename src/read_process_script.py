33a92e44eef83239e8a5b1a78afb841b7371ed22
Ahmed Rizk <ahmadamrizk@gmail.com>
2022-03-27 22:35:28 -0700

    Helper functions created - read_process_script
    Split the read_process script into helper functions, so that all functions are called in main at the bottom

--- src/read_process_script.py
+++ src/read_process_script.py
@@ -7,9 +7,11 @@
  Usage:
 
  Options:
- --read_path=<read_path>    '''Path of the file to read'''
- --out_path=<out_path>    '''path to the cleaned dataset object created'''
- --cols_kept=<cols_kept>  '''array of columns to keep from dataset'''
+ --read_path=<read_path>    Path of the file to read
+ --out_path=<out_path>    path to the cleaned dataset object created
+ --processed_path=<processed_path>    path to the processed dataset object created, before the training/testing split
+ --train_path=<train_path> path to the training data
+ --test_path=<test_path> path to the testing data
 
 ''' 
 
@@ -21,33 +23,61 @@ np.random.seed(1)
 
 opt = docopt(__doc__)
 
-def main(read_path, out_path, cols_kept):
-
+def read_trim(read_path, chosen_cols):
     data = pd.read_csv(read_path, header=None)
-    first_five = data.head() # save
-    reduced_data = data[cols_kept]
-    reduced_info = reduced_data.info() # save
-    reduced_describe = reduced_data.describe # save
-    data.to_csv(out_path, index = False)
-    reduced_data = reduced_data.drop(columns = 'USHRWK')  # too specific
-    reduced_data = reduced_data.loc[reduced_data['ATINC'] != 99999996]  # too specific
-    
-    
-    #### The previous section is up to "data splitting and processing"
-    ### The following is from "data splitting and processing" till the first model
-    
-    processed = reduced_data[["EFINVA","EFSIZE","EFMJIE"]]
-    binary_fea =["EFMJIE"]
-    cate_fea = ["EFSIZE"]
+    reduced_data = data[chosen_cols]
+    return reduced_data
+
+
+def process(data, processed_path):
+    data = data.drop(columns = 'USHRWK') 
+    data = data.loc[reduced_data['ATINC'] != 99999996] 
+    processed = data[["EFINVA","EFSIZE","EFMJIE"]]
+    # write to file data
+    return processed    # should I split into 3?
+
+
+
+def train_test_drop(data, dropped_col, train_path, test_path):
+    X_train, Y_train, X_test, Y_test = split_drop(data, 0.3, 123, dropped_col)
+    # write to file data/train and data/test
+    return X_train, Y_train, X_test, Y_test
+
+
+def create_pipeline(binary, category):
+    binary_fea = binary
+    cate_fea = category
     cate_trans = make_pipeline(OrdinalEncoder(categories = [[1, 2, 3, 4, 5, 6, 7]], dtype=int))
     binary_trans = make_pipeline(OneHotEncoder(drop="if_binary"))
     preprocessor = make_column_transformer(
         (binary_trans, binary_fea),
         (cate_trans,cate_fea)
 )
-    train_processed = preprocessor.fit_transform(X_train)
-    pd.DataFrame(train_processed, columns = ["EFMJIE","EFSIZE"])
+    return preprocessor
+    
+    
+def create_reduced_dataframe(train, preprocessor):
+    
+    train_processed = preprocessor.fit_transform(train)
+    
 
+
+
+def main(read_path, out_path, processed_path, train_path, test_path):
+    reduced_data = read_trim(read_path, ['EFSIZE', 'USHRWK', 'ATINC', 'HLEV2G', 'EFINVA', 'EFMJIE', 'EFATINC', 'EFMJSI'])
+    processed = process(reduced_data)
+    X_train, Y_train, X_test, Y_test = train_test_drop(processed, "EFINVA")
+    preprocessor = create_pipeline("EFMJIE", "EFSIZE")
+    reduced_dataframe = create_reduced_dataframe(X_train, preprocessor)
+    
+    
+    
+
+    
+    
+
+    reduced_dataframe.to_csv(out_path, index = False)
+    
     
 main(opt["--read_path"], opt["--out_path"], opt[["--cols_kept"]])
 
