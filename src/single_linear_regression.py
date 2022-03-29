dee81a49fdc0e1877c84b23a2215711f29e49870
Shiyang Zhang <aminocidtt@outlook.com>
2022-03-28 22:29:47 -0700

    Make some helper function to make my code more clean and tidy.

--- src/single_linear_regression.py
+++ src/single_linear_regression.py
@@ -18,13 +18,24 @@ import src.hyperparameter_optimization as ho
 opt = docopt(__doc__)
 
 
+def ridge_pipline(processor,alpha):
+    # This function is a helper function to create a specific case for pipline creating a ridge pipline
+    # Parameter:
+    #   --processor:
+    ridge_pipeline = make_pipeline(processor, Ridge(alpha=alpha))
+    return ridge_pipeline
 
+def cross_validation(ridgepip,xtrain,ytrain):
+    cross_validate(ridgepip, xtrain, ytrain, cv=10, return_train_score=True)
+
+def write_csv(pd,out_dir):
+    pd.to_csv(out_dir, index=True)
 
 
 def main(xtrain, ytrain):
     ridge_pipeline = make_pipeline(ho.preprocessor, Ridge(alpha=ho.best_alpha))
     cv_ridge = pd.DataFrame(cross_validate(ridge_pipeline, xtrain, ytrain, cv=10, return_train_score=True))
-    cv_ridge.to_csv("../data/cv_ridge.csv", index = True)
+    write_csv(cv_ridge,"../data/cv_ridge.csv")
 
 
 if __name__ == "__main__":
