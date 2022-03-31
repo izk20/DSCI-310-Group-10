ed53496907df81c2b774a287cbd26b5ae6c298cf
harry <harryzhang957@gmail.com>
2022-03-31 15:49:45 -0700

    Try to deal with some debug on the hyperparameter part, but still need more action on it

--- src/hyperparameter_optimization.py
+++ src/hyperparameter_optimization.py
@@ -2,13 +2,14 @@
 # date: 2022-03-25
 
 """
-Usage: src/read_process_script.py --xtrain=<xtrain> --ytrain=<ytrain> --variables=<variables>
+Usage: src/read_process_script.py --xtrainpath=<xtrainpath> --ytrainpath=<ytrainpath> --variables=<variables>
 
 Options:
---xtrain=<xtrain>:    csv file previously saved in the previous script the training data for the x-axis of ridge regression
---ytrain=<ytrain>:     csv file previously saved int the previous script the training data for the y-axis of ridge regression
---variables = <variables>:  A list of variables which contains both binary and categories feature of processor.
+--xtrainpath=<xtrainpath>:    csv file previously saved in the previous script the training data for the x-axis of ridge regression
+--ytrainpath=<ytrainpath>:     csv file previously saved int the previous script the training data for the y-axis of ridge regression
+--variables=<variables>:  A list of variables which contains both binary and categories feature of processor.
 """
+
 import pandas as pd
 from docopt import docopt
 import numpy as np
@@ -16,6 +17,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 from sklearn.compose import make_column_transformer
 from analysis.alpha_tuning import ridge_alpha_tuning
+import pickle
 
 opt = docopt(__doc__)
 
@@ -30,19 +32,22 @@ def make_processor(binary_fea, cate_fea):
 
 def main(xtrainpath, ytrainpath, variables):
     variables = [x for x in variables.split(',')]
-    # print(args)
+    print(variables)
     xtrain = pd.read_csv(xtrainpath)
+    print(xtrain.head())
     ytrain = pd.read_csv(ytrainpath)
-    # raise ImportError(xtrain)
-    preprocessor = make_processor(variables[0], variables[1])
-    # raise ImportError(variables[0])
-    train_processed = preprocessor.fit_transform(xtrain,ytrain)
-    alphas = list(10.0 ** np.arange(-2, 5, 1))
-    best_alpha = ridge_alpha_tuning(alphas, preprocessor, xtrain, ytrain)
-    print(best_alpha)
-    train_processed.to_csv(train_processed, "train_processed")
-    return best_alpha,preprocessor
+    print(ytrain)
+    # # raise ImportError(type(ytrain.loc[:,[0]]))
+    preprocessor = make_processor("EFSIZE","EFMJIE")
+    # raise ImportError(preprocessor)
+    train_processed = preprocessor.fit_transform(xtrain)
+    # alphas = list(10.0 ** np.arange(-2, 5, 1))
+    # best_alpha = ridge_alpha_tuning(alphas, preprocessor, xtrain, ytrain)
+    # print(best_alpha)
+    # train_processed.to_csv( "results/train_processed.csv")
+    # best_alpha.to_csv("results/best_alpha.csv")
+    # pickle.dump(preprocessor, open("results/preprocessor.pickle", "wb"))
 
 
 if __name__ == "__main__":
-    main(opt["--xtrain"], opt["--ytrain"],opt["--variables"])
+    main(opt["--xtrainpath"], opt["--ytrainpath"],opt["--variables"])
--- src/single_linear_regression.py
+++ src/single_linear_regression.py
@@ -2,19 +2,21 @@
 # date: 2022-03-25
 
 """
-Usage: src/hyperparameter_optimization.py --xtrain=<xtrain> --ytrain=<ytrain>
+Usage: src/hyperparameter_optimization.py --xtrain=<xtrainpath> --ytrain=<ytrainpath>
 
 Options:
---xtrain=<xtrain>:    csv file previously saved in the previous script the training data for the x-axis of ridge regression
---ytrain=<ytrain>:     csv file previously saved int the previous script the training data for the y-axis of ridge regression
+--xtrain=<xtrainpath>:    csv file previously saved in the previous script the training data for the x-axis of ridge regression
+--ytrain=<ytrainpath>:     csv file previously saved int the previous script the training data for the y-axis of ridge regression
 """
 
 from docopt import docopt
+import pickle
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import cross_validate
 from sklearn.pipeline import make_pipeline
-import src.hyperparameter_optimization as ho
+import matplotlib.pyplot as plt
 
 opt = docopt(__doc__)
 
@@ -32,11 +34,25 @@ def cross_validation(ridgepip,xtrain,ytrain):
 def write_csv(pd,out_dir):
     pd.to_csv(out_dir, index=True)
 
+def make_plot(cv_ridge):
+    ridge = plt.plot(np.arange(len(cv_ridge)),
+                     cv_ridge['test_score'],
+                     '-0')
+    plt.title('Figure 3: RidgeCV Folds = 10')
+    plt.xlabel('CV Fold Iterations')
+    plt.ylabel('CV Accuracy')
+    return ridge
 
-def main(xtrain, ytrain):
-    ridge_pipeline = make_pipeline(ho.preprocessor, Ridge(alpha=ho.best_alpha))
+def main(xtrainpath, ytrainpath):
+    xtrain = pd.read_csv(xtrainpath)
+    ytrain = pd.read_csv(ytrainpath)
+    preprocessor = pickle.load(open("results/preprocessor.pickle", "rb"))
+    best_alpha = pd.read_csv("results/best_alpha.csv")
+    ridge_pipeline = make_pipeline(preprocessor, Ridge(alpha=best_alpha))
     cv_ridge = pd.DataFrame(cross_validate(ridge_pipeline, xtrain, ytrain, cv=10, return_train_score=True))
     write_csv(cv_ridge,"../data/cv_ridge.csv")
+    cv_plot = make_plot(cv_ridge)
+    cv_plot.savefig('cv_plot.png')
 
 
 if __name__ == "__main__":
