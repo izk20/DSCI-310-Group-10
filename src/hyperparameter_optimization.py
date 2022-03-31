346b03090ec27992423ceb7e4efb0289dc8a352c
harry <harryzhang957@gmail.com>
2022-03-30 21:30:58 -0700

    fix some bugs more to work on

--- src/hyperparameter_optimization.py
+++ src/hyperparameter_optimization.py
@@ -2,14 +2,14 @@
 # date: 2022-03-25
 
 """
-Usage: src/read_process_script.py --xtrain=<xtrain> --ytrain=<ytrain> --variables = <variables>
+Usage: src/read_process_script.py --xtrain=<xtrain> --ytrain=<ytrain> --variables=<variables>
 
 Options:
 --xtrain=<xtrain>:    csv file previously saved in the previous script the training data for the x-axis of ridge regression
 --ytrain=<ytrain>:     csv file previously saved int the previous script the training data for the y-axis of ridge regression
 --variables = <variables>:  A list of variables which contains both binary and categories feature of processor.
 """
-
+import pandas as pd
 from docopt import docopt
 import numpy as np
 from sklearn.pipeline import make_pipeline
@@ -28,13 +28,17 @@ def make_processor(binary_fea, cate_fea):
     return preprocessor
 
 
-def main(xtrain, ytrain, variables):
-    preprocessor = make_processor(variables[1], variables[2])
+def main(xtrainpath, ytrainpath, variables):
+    xtrain = pd.read_csv(xtrainpath)
+    ytrain = pd.read_csv(ytrainpath)
+    # raise ImportError(ytrain)
+    preprocessor = make_processor(variables[0], variables[1])
+    # raise ImportError(variables)
     train_processed = preprocessor.fit_transform(xtrain)
     alphas = list(10.0 ** np.arange(-2, 5, 1))
     best_alpha = ridge_alpha_tuning(alphas, preprocessor, xtrain, ytrain)
     print(best_alpha)
-    train_processed.to_csv()
+    train_processed.to_csv(train_processed, "train_processed")
     return best_alpha,preprocessor
 
 
